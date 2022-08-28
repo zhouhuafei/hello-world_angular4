af73292199b22d6513442f51f67f76ef0b6e9fbf
zhouhuafei <1123486116@qq.com>
2022-08-28 19:46:06 +0800

    feat: 🎸 增加模块热更新功能

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,17 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+} else {
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => {
+      const domStyles: any = document.head.querySelectorAll('style');
+      const arrStyles: any = Array.prototype.slice.call(domStyles);
+      arrStyles
+        .filter((style: any) => style.innerText.indexOf('_ng') !== -1)
+        .map((el: any) => document.head.removeChild(el));
+    });
+  }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
