8b21b965c1e577483a0d3d1eb58d2f8457b5b82c
Angular CLI <angular-cli@angular.io>
2022-08-28 18:36:19 +0800

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { HelloWorldAngular4Page } from './app.po';
+
+describe('hello-world-angular4 App', () => {
+  let page: HelloWorldAngular4Page;
+
+  beforeEach(() => {
+    page = new HelloWorldAngular4Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
